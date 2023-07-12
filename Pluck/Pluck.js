2638f665393dcbf5d996fc8b0aa59537b62d84e0
PavlovVitalii <60616724+PavlovVitalii@users.noreply.github.com>
2023-07-12 13:04:13 +0300

    Pluck first commit

---
+++ Pluck/Pluck.js
@@ -0,0 +1,8 @@
+//Implement a function which takes a sequence of objects and a property name,
+// and returns a sequence containing the named property of each object.
+//If an object is missing the property, you should just leave it as
+// undefined/None in the output array.
+
+function pluck(objs, name) {
+
+}
