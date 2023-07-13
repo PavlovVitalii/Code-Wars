0ee9404e926d59c990520edf554c5164d6c98531
PavlovVitalii <60616724+PavlovVitalii@users.noreply.github.com>
2023-07-13 14:40:46 +0300

    X marks the spot final commit

--- X marks the spot!/XMarksTheSpot.js
+++ X marks the spot!/XMarksTheSpot.js
@@ -1,16 +1,26 @@
-//Write a function x(n) that takes in a number n and returns an nxn array with an X in the middle. 
+//Write a function x(n) that takes in a number n and returns an nxn array with an X in the middle.
 //The X will be represented by 1's and the rest will be 0's.E.g.
 
 function x(n) {
-    const arr = new Array(n).fill(new Array(n).fill(0));
-    let left = 0;
-    let right = arr.length;
-    let midle 
-    for (let index = 0; index < arr.length; index++) {
-        arr[index];
-        
-    }
-    return arr;
- }
+  const arr = [];
+  let arrLength;
 
- console.log(x(1));
+  for (let i = 0; i < n; i++) {
+    arr[i] = [];
+    for (let j = 0; j < n; j++) {
+      arr[i][j] = 0;
+    }
+  }
+
+  arrLength = arr.length - 1;
+
+  for (let index = 0; index < arr.length; index++) {
+    arr[index][index] = 1;
+    arr[index][arrLength] = 1;
+    arrLength--;
+  }
+
+  return arr;
+}
+
+
