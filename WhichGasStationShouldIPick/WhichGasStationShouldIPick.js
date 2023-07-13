ee03880962bb8cc804efa2bf9013be93e577ef5e
PavlovVitalii <60616724+PavlovVitalii@users.noreply.github.com>
2023-07-13 14:55:05 +0300

    Which Gas Station should I pick? first commit

---
+++ WhichGasStationShouldIPick/WhichGasStationShouldIPick.js
@@ -0,0 +1,18 @@
+//You have to fill up your gas and there are multiple gas stations with different prices
+// and different distance to you. Sometimes it is cheaper to drive to a more distant gas station, 
+//because the prices are cheaper!
+
+// Your tank can contain 60l at maximum.
+// You always fill your tank full
+// Calculate the current fuel in tank with the actual price of the gas stations
+// Your task:
+// Given an object with multiple gas stations, your currentFuel as integer between 0 and 60 and the fuel consumption of your car (l/100km, float) - find the cheapest gas station and return the name of the gas station!
+
+// Return undefined (in JS) | None (in Python) if there are no gas station or your fuel is not enough to reach one!
+
+// No need to test for invalid input!
+// Remember: You also need fuel to drive to the gas station! The way back home should also be considered :)
+
+function gasStation(obj, currentFuel, fuelConsumption){
+    
+  }
