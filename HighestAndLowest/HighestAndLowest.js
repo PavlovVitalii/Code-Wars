6456b61a8407095231300da72d8f922389cc938b
PavlovVitalii <60616724+PavlovVitalii@users.noreply.github.com>
2023-07-14 19:47:16 +0300

    Highest and Lowest final commit

--- HighestAndLowest/HighestAndLowest.js
+++ HighestAndLowest/HighestAndLowest.js
@@ -6,7 +6,9 @@
 //There will always be at least one number in the input string.
 //Output string must be two numbers separated by a single space, and highest number is first.
 
-
-function highAndLow(numbers){
-    
-  }
+function highAndLow(numbers) {
+  const arrNumbers = numbers.split(" ").map((item) => Number(item));
+  const minNumber = Math.min(...arrNumbers);
+  const maxNumber = Math.max(...arrNumbers);
+  return `${maxNumber} ${minNumber}`;
+}
