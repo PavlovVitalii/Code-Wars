b69dca333afc5f0bccfd3924bcf7856bcbf7a3f4
PavlovVitalii <60616724+PavlovVitalii@users.noreply.github.com>
2023-07-18 12:04:06 +0300

    Decode the Morse code final commit

--- DecodeTheMorseCode/DecodeTheMorseCode.js
+++ DecodeTheMorseCode/DecodeTheMorseCode.js
@@ -1,11 +1,11 @@
 //In this kata you have to write a simple Morse code decoder.
-// While the Morse code is now mostly superseded by voice and digital data communication channels, 
+// While the Morse code is now mostly superseded by voice and digital data communication channels,
 //it still has its use in some applications around the world.
-//The Morse code encodes every character as a sequence of "dots" and "dashes". 
-//For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. 
-//The Morse code is case-insensitive, traditionally capital letters are used. 
-//When the message is written in Morse code, a single space is used to separate the character 
-//codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse 
+//The Morse code encodes every character as a sequence of "dots" and "dashes".
+//For example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−.
+//The Morse code is case-insensitive, traditionally capital letters are used.
+//When the message is written in Morse code, a single space is used to separate the character
+//codes and 3 spaces are used to separate words. For example, the message HEY JUDE in Morse
 //code is ···· · −·−−   ·−−− ··− −·· ·.
 
 //NOTE: Extra spaces before or after the code have no meaning and should be ignored.
@@ -23,28 +23,81 @@
 //The Morse code table is preloaded for you as a dictionary, feel free to use it:
 
 const MORSE_CODE = {
-    '-.-.--': '!',      '.-..-.': '"', '...-..-': '$',
-    '.-...': '&',       '.----.': "'", '-.--.': '(',
-    '-.--.-': ')',      '.-.-.': '+',  '--..--': ',',
-    '-....-': '-',      '.-.-.-': '.', '-..-.': '/',
-    '-----': '0',       '.----': '1',  '..---': '2',
-    '...--': '3',       '....-': '4',  '.....': '5',
-    '-....': '6',       '--...': '7',  '---..': '8',
-    '----.': '9',       '---...': ':', '-.-.-.': ';',
-    '-...-': '=',       '..--..': '?', '.--.-.': '@',
-    '.-': 'A',          '-...': 'B',   '-.-.': 'C',
-    '-..': 'D',         '.': 'E',      '..-.': 'F',
-    '--.': 'G',         '....': 'H',   '..': 'I',
-    '.---': 'J',        '-.-': 'K',    '.-..': 'L',
-    '--': 'M',          '-.': 'N',     '---': 'O',
-    '.--.': 'P',        '--.-': 'Q',   '.-.': 'R',
-    '...': 'S',         '-': 'T',      '..-': 'U',
-    '...-': 'V',        '.--': 'W',    '-..-': 'X',
-    '-.--': 'Y',        '--..': 'Z',   '..--.-': '_',
-    '...---...': 'SOS'
+  "-.-.--": "!",
+  ".-..-.": '"',
+  "...-..-": "$",
+  ".-...": "&",
+  ".----.": "'",
+  "-.--.": "(",
+  "-.--.-": ")",
+  ".-.-.": "+",
+  "--..--": ",",
+  "-....-": "-",
+  ".-.-.-": ".",
+  "-..-.": "/",
+  "-----": "0",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "---...": ":",
+  "-.-.-.": ";",
+  "-...-": "=",
+  "..--..": "?",
+  ".--.-.": "@",
+  ".-": "A",
+  "-...": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  "..-.": "F",
+  "--.": "G",
+  "....": "H",
+  "..": "I",
+  ".---": "J",
+  "-.-": "K",
+  ".-..": "L",
+  "--": "M",
+  "-.": "N",
+  "---": "O",
+  ".--.": "P",
+  "--.-": "Q",
+  ".-.": "R",
+  "...": "S",
+  "-": "T",
+  "..-": "U",
+  "...-": "V",
+  ".--": "W",
+  "-..-": "X",
+  "-.--": "Y",
+  "--..": "Z",
+  "..--.-": "_",
+  "...---...": "SOS",
 };
 
-decodeMorse = function(morseCode){
-    const words = morseCode.split("   ").split(" ");
-   console.log();
-  }
+decodeMorse = function (morseCode) {
+  const wordsMorse = morseCode.trim().split("   ");
+
+  wordsMorse.forEach((element, index) => {
+    wordsMorse[index] = element.split(" ");
+  });
+
+  let result = wordsMorse
+    .map((el) => {
+      return el
+        .map((el) => {
+          return MORSE_CODE[el];
+        })
+        .join("");
+    })
+    .join(" ");
+
+  return result;
+};
+
+
