8f9d8c9fa3ca4bfe611e81cc8b1307b17b79372a
PavlovVitalii <60616724+PavlovVitalii@users.noreply.github.com>
2023-07-30 21:44:59 +0300

    Regex count lowercase letters commit

---
+++ RegexCountLowercaseLetters/RegexCountLowercaseLetters.js
@@ -0,0 +1,8 @@
+// Your task is simply to count the total number of lowercase letters in a string.
+
+function lowercaseCount(str){
+    const lowercaseLetters = str.match(/[a-z]+/g);
+    return lowercaseLetters === null ? 0 : lowercaseLetters.join("").length;
+}
+
+console.log(lowercaseCount("b]iva)"));
