8975372e2199540bbdc3c1842fb8772628b91a29
PavlovVitalii <60616724+PavlovVitalii@users.noreply.github.com>
2025-04-24 14:19:51 +0300

    Build Tower Advanced first commit

---
+++ Build Tower Advanced/BuildTowerAdvanced.js
@@ -0,0 +1,9 @@
+// Build Tower by the following given arguments:
+
+// number of floors (integer and always greater than 0)
+// block size (width, height) (integer pair and always greater than (0, 0))
+
+
+function towerBuilder(nFloors, nBlockSz) {
+    // build here
+  }
