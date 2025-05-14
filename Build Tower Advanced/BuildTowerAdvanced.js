1e5b6c2989ce51cb675f413ce7401c5a5d0ea746
PavlovVitalii <60616724+PavlovVitalii@users.noreply.github.com>
2025-05-14 16:08:45 +0300

    Build Tower Advanced final commit

--- Build Tower Advanced/BuildTowerAdvanced.js
+++ Build Tower Advanced/BuildTowerAdvanced.js
@@ -3,7 +3,31 @@
 // number of floors (integer and always greater than 0)
 // block size (width, height) (integer pair and always greater than (0, 0))
 
-
 function towerBuilder(nFloors, nBlockSz) {
-    // build here
+  let [startWidthBlok, blockHeight] = nBlockSz;
+  let widthTower = startWidthBlok;
+  let widthBlok = null;
+  let tower = [];
+
+  for (let index = 1; index < nFloors; index++) {
+    widthTower = widthTower + startWidthBlok * 2;
   }
+
+  widthBlok = widthTower;
+
+  for (let index = 0; index < nFloors; index++) {
+    for (let j = 0; j < blockHeight; j++) {
+      tower.unshift(
+        " ".repeat((widthTower - widthBlok) / 2) +
+          "*".repeat(widthBlok) +
+          " ".repeat((widthTower - widthBlok) / 2)
+      );
+    }
+
+    widthBlok = widthBlok - startWidthBlok * 2;
+  }
+
+  return tower;
+}
+
+
