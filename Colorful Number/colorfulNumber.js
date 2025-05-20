276dfe8edfb9ff26760b35b667e96b5c4738565e
PavlovVitalii <60616724+PavlovVitalii@users.noreply.github.com>
2025-05-20 16:06:05 +0300

    Colorful Number first commit

---
+++ Colorful Number/colorfulNumber.js
@@ -0,0 +1,5 @@
+//Determine whether a non-negative integer number is colorful or not.
+//263 is a colorful number because [2, 6, 3, 2*6, 6*3, 2*6*3] are all different;
+//  whereas 236 is not colorful, because [2, 3, 6, 2*3, 3*6, 2*3*6] has 6 twice.
+//So take all consecutive subsets of digits, take their products, and ensure 
+// all the products are different.
